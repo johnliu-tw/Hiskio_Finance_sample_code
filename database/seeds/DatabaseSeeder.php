231f657cfb3400c3a43d5f4776eee64237e21d53
johnliu <johnliutw@hotmail.com>
2020-07-18 15:50:37 +0800

    STEP 0 - create product schema && frontend page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Product::updateOrCreate(['id' => 1, 'name' => '測試商品1', 'price' => 200]);
+        Product::updateOrCreate(['id' => 2, 'name' => '測試商品2', 'price' => 500]);
     }
 }
--- resources/views/pure_purchases/index.blade.php
+++ resources/views/pure_purchases/index.blade.php
@@ -1,3 +1,15 @@
-<form action="/purePurchases" method="post">
-  <input type="submit" value="純粹的送出">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<form class="container mx-auto w-25 pt-5" action="/purePurchases" method="post">
+  <select name='productId' class="form-control form-control mt-3">
+    @foreach( $products as $product )
+      <option value='{{ $product->id }}'>{{ $product->name }}</option>
+    @endforeach
+  </select>
+  <select name='method' class="form-control form-control mt-3">
+    <option value='atm'>ATM</option>
+    <option value='credit'>信用卡</option>
+  </select>
+  <button type="submit" class="btn btn-primary mt-3">純粹的送出</button>
 </form>
