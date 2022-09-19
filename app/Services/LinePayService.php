50f13417e3bd1936edbca85f8b5e8f4c50865b22
johnliu <johnliutw@hotmail.com>
2022-09-19 20:34:24 +0800

    Plus: add LinePay Service

--- app/Services/LinePayService.php
+++ app/Services/LinePayService.php
@@ -2,14 +2,15 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 
 class LinePayService
 {
     private $channelId;
     private $channelSecretKey;
     private $nonce;
+    private $baseUrl;
 
 
     // 初始化，設定 data, mac, signature 和加密用的 dataString
@@ -17,81 +18,90 @@ class LinePayService
     {
         $this->channelId = ($channelId != null ? $channelId : env('LINE_CHANNEL_ID'));
         $this->channelSecretKey = ($channelSecretKey != null ? $channelSecretKey : env('LINE_SECRETKEY'));
-        $this->nonce = $this->gen_uuid();
+        $this->nonce = $this->genUuid();
         $this->baseUrl = env('LINE_URL');
     }
 
-    public function payment()
+    public function send()
     {
         $body = $this->setBody();
         $url = '/v3/payments/request';
-        
         $header = $this->setHeader($url, $body);
         $response = Http::withHeaders($header)->post($this->baseUrl.$url, $body);
-        return $response->body();
+        dump($response->body());
     }
 
     public function confirm($tranId)
     {
-        $body = array(
-          'amount' => 3000,
-          'currency' => 'TWD'
-        );
+        $body = array('amount' => 3000, 'currency'=>'TWD');
         $url = '/v3/payments/'.$tranId.'/confirm';
-        
         $header = $this->setHeader($url, $body);
         $response = Http::withHeaders($header)->post($this->baseUrl.$url, $body);
-        return $response->body();
+        dump($response->body());
     }
+
 
     public function setHeader($url, $body)
     {
         $signatureData = $this->channelSecretKey . $url . json_encode($body) . $this->nonce;
         $signature = base64_encode(hash_hmac('sha256', $signatureData, $this->channelSecretKey, true));
-       
         return [
           'Content-Type' => 'application/json',
           'X-LINE-ChannelId' => $this->channelId,
-          'X-LINE-Authorization-Nonce' => $this->nonce ,
+          'X-LINE-Authorization-Nonce' => $this->nonce,
           'X-LINE-Authorization' => $signature
         ];
     }
-
     public function setBody()
     {
         return array(
-          "amount" => 3000,
-          "currency" => "TWD",
-          "orderId" => strval(Carbon::now()->timestamp),
-            'packages'=> [
+        'amount' => 3000,
+        'currency' => 'TWD',
+        'orderId' => strval(Carbon::now()->timestamp),
+        'packages' => [
+          [
+            'id' => '10',
+            'amount' => 3000,
+            'name' => 'test',
+            'products' => [
               [
-                "id" => strval(10),
-                "amount" => 3000,
-                'products' => [[
-                  "name" => "testProducts",
-                  "quantity" => 2,
-                  "price" => 1500
-                ]
-                ]
+                'name' => 'testProduct',
+                'quantity' => 2,
+                'price' => 1500
               ]
-            ],
-            'redirectUrls' =>
-            [
-              'confirmUrl' => env('LINE_CONFIRM_URL'),
-              'cancelUrl' => env('LINE_CANCEL_URL')
-            ]);
+            ]
+          ]
+              ],
+              'redirectUrls' => [
+                'confirmUrl' => env('LINE_CONFIRM_URL'),
+                'cancelUrl' => env('LINE_CANCEL_URL')
+              ]
+              );
     }
 
-    public function gen_uuid()
+    public function genUuid()
     {
         return sprintf(
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+      // 32 bits for "time_low"
+      mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
+  
+      // 16 bits for "time_mid"
+      mt_rand(0, 0xffff),
+  
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand(0, 0x0fff) | 0x4000,
+  
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand(0, 0x3fff) | 0x8000,
+  
+      // 48 bits for "node"
+      mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
             mt_rand(0, 0xffff)
         );
